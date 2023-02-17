bc0edd9a18d18de706414a019147186050983b83
rinkuberde <rinkuberde@gmail.com>
2023-02-17 20:54:02 +0530

    [Rinku] - Welcome Message

---
+++ EmployeePayrollService-ADO.Net/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace EmployeePayrollService_ADO.Net
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to EmployeePayrollService-ADO.Net");
+        }
+    }
+}
