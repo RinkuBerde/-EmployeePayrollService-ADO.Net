506899a1eeaf6f9366dd3014bfd11d1ebd99ad52
rinkuberde <rinkuberde@gmail.com>
2023-02-17 21:22:02 +0530

    [Rinku] - Create Database

---
+++ EmployeePayrollService-ADO.Net/EmployeeRepo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeePayrollService_ADO.Net
+{
+    public class EmployeeRepo
+    {
+        public static string connectionString = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=PayrollService;Integrated Security=True";
+        SqlConnection connection = new SqlConnection(connectionString);
+    }
+}
